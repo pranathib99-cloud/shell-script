45f5bb94b688d6ac942439c0eb434f487b0de7f8
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-09-26 18:07:13 +0530

    message

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "please enter your pin "
-read PIN-NUMBER # read is used to take input from user
+read PIN_NUMBER # read is used to take input from user
 
 echo "youe pin is : $PIN_NUMBER"
