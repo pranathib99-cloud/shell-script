5ca43a1df6c5e0151d14cc4c75bbc84ff1380aaf
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-09-26 17:10:23 +0530

    message

---
+++ 02-converstion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh:: hi how are you"
+echo "suresh:: i am fine ramesh ,how are you"
+echo "ramesh:: what ur learning recently"
+echo "suresh:: i am learning shell scripting ramesh"
---
+++ hello-world.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+hello() {
+    echo "Hello, World!"
+}
+
+# IS commit of , cant be added just for information
