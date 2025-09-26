f2991905e3349c2700c5ee7e66235c2502c91e4e
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-09-26 19:13:33 +0530

    message

--- 08-variable.sh
+++ 08-variable.sh
@@ -11,3 +11,7 @@ echo "script name is : $0"
 echo "cureent dircetory is : $PWD"
 echo "home directory is : $HOME"
 echo "user name is : $USER"
+echo "PID of current script is : $$"
+
+sleep 50 & # sleep for 50 seconds
+echo "PID of last background process is : $!"
