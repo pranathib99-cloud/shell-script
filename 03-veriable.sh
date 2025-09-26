ffc464e4ec5915e9ff51ad807373a711fc8bf0a0
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-09-26 17:19:56 +0530

    message

---
+++ 03-veriable.sh
@@ -0,0 +1,8 @@
+#!/bin/basH
+PERSON1=JHON       # VAR-NAME=VALUE
+PERSION2=RAMESH
+
+echo "$PERSION1:: hi how are you"
+echo "$PERSON2:: i am fine $PERSON1 ,how are you"
+echo "$PERSON1:: what ur learning recently"
+echo "$PERSION2:: i am learning shell scripting $PERSON1"
