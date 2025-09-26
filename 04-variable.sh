74e47d4568937c8501121468bb2aa33787e1e21a
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-09-26 17:59:08 +0530

    message

---
+++ 04-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hi how are you"
+echo "$PERSON2:: i am fine $PERSON1 ,how are you"
+echo "$PERSON1:: what ur learning recently"
+echo "$PERSON2:: i am learning shell scripting $PERSON1"
