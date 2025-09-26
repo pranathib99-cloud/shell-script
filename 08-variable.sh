bf59c445d85a87a1393fd02a08ce06083dead44b
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-09-26 19:08:25 +0530

    message

--- 08-variable.sh
+++ 08-variable.sh
@@ -4,3 +4,10 @@ PERSON1=sai    # VAR-NAME=VALUE
 
 echo "all variables passed to the script are : $@"
 echo "all variable passed to the script is : $*"
+
+# self special variable
+
+echo "script name is : $0"
+echo "cureent dircetory is : $PWD"
+echo "home directory is : $HOME"
+echo "user name is : $USER"
