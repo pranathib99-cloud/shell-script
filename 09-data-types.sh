2828f37304ff4fc011afbddb0de195835768a22f
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-10-03 10:57:18 +0530

    variable

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -9,6 +9,9 @@ NAME=devops  #string
 sum=$((NUMBER1 + NUMBER2 + NAME))  #arithmetic operation
 echo "Sum: $sum"
 
+#size =4 , max size = 3
+
 LEADERS=( "MODi" "PUTHIN" "kcr" "ktr")
 
 echo "all leaders: ${LEADERS[@]}"  #@ to get all values of names
+echo "first leader:${LEADERS[2]}"  #index starts from 0 #to get specific value {now 2nd index value is puthin}
