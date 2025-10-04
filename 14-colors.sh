20a329264faa1a4614a471b6d913defa23fa1405
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-10-04 12:14:58 +0530

    condition

--- 14-colors.sh
+++ 14-colors.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 R="\e[31m"  #Red
-
 G="\e[32m" #Green
-
 Y="\e[33m"] #Yellow
-
 N="\e[0m"  #No Color white
-
 echo -e "$G  hello world $N"            #-e is used to enable to run commands
 echo  "check the color"                            #-n is used to avoid new line
---
+++ 15-instalation-ad-col.sh
@@ -0,0 +1,42 @@
+USERID=$(id -u)     #get user id of current user
+R="\e[31m"  #Red
+G="\e[32m" #Green
+Y="\e[33m"] #Yellow
+N="\e[0m"  #No Color white
+
+if [ $USERID -ne 0 ]; then 
+    echo " ERROR:: please run this script as root privileges"
+    exit 1          #failure is other then 0
+fi 
+VALIDATE(){                  #Functions recevive input to /Aguments just like scripts arguments
+    if [ $1 -ne 0 ]; then
+        echo "ERROR :: $2 .. $R failure $N"
+        exit 1           #exit with failure status 
+    else
+        echo "installing $2 ..$G success $N"
+    fi
+}
+
+
+
+dnf install mysql -y
+#install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+else
+    echo -e "mysql is already exit ... $Y success $N"
+fi
+      
+VALIDATE $1 "MYSQL INSTALLATION"
+
+dnf install nginx -y
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+else
+    echo -e "ngnix is already exit ... $Y success $N"
+fi
+      
+VALIDATE $1 "nginx INSTALLATION
+
+dnf install python3 -y
+VALIDATE $1 "python3 INSTALLATION"
