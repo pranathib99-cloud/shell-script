ba67e8d6338357c61e224fc75e611ee57a1da211
pranathi.b99-cloud <pranathi.b99@gmail.com>
2025-10-04 10:51:50 +0530

    condition

--- 12.installation.sh
+++ 12.installation.sh
@@ -5,12 +5,15 @@ USERID=$(id -u)     #get user id of current user
 
 if [ $USERID -ne 0 ]; then 
     echo " ERROR:: please run this script as root privileges"
+    exit 1          #failure is other then 0
 fi 
+
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR :: mysql installation is failure"
+    exit 1           #exit with failure status 
 else
     echo "mysql install success"
 fi
