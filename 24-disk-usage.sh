34f583d8fcaa5a4d5852bf2f4abe7a1d79a16267
pranathi.b99-cloud <pranathi.b99@gmail.com>
2026-01-11 16:39:05 +0530

    usage

--- 24-disk-usage.sh
+++ 24-disk-usage.sh
@@ -25,7 +25,8 @@ DISK_THRESHOLD=2 # in project we keep it as 75
 IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 MESSAGE=""
 
-while IFS= read -r line ;do
+while IFS= read -r line ;
+do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
