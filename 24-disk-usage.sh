806a7deb263d1aca6cf48cf17183a6e11645b9c4
pranathi.b99-cloud <pranathi.b99@gmail.com>
2026-01-11 16:38:19 +0530

    usage

--- 24-disk-usage.sh
+++ 24-disk-usage.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep -v Filesystem)
+# DISK_USAGE=$(df -hT | grep -v Filesystem)
 
-DISK_THRESHOLD=2 #in projects we keep it as 75
-MESSAGE=""
-IP_ADDRESS=$( curl -s http://169.254.169.254/latest/meta_data/local_ipv4)
+# DISK_THRESHOLD=2 #in projects we keep it as 75
+# MESSAGE=""
+# IP_ADDRESS=$( curl -s http://169.254.169.254/latest/meta_data/local_ipv4)
 
 
 
-while read -r line; do
-  USAGE=$(echo "$line" | awk '{ print $6 }' | cut -d "%" -f1)
-  PARTITION=$(echo "$line" | awk '{ print $7 }')
+# while read -r line; do
+#   USAGE=$(echo "$line" | awk '{ print $6 }' | cut -d "%" -f1)
+#   PARTITION=$(echo "$line" | awk '{ print $7 }')
   
-  if [ "$USAGE" -ge "$DISK_THRESHOLD" ]; then
-     MESSAGE+="high disk usage $PARTITION=$USAGE% <br> "
-  fi
-done <<< "$DISK_USAGE"
+#   if [ "$USAGE" -ge "$DISK_THRESHOLD" ]; then
+#      MESSAGE+="high disk usage $PARTITION=$USAGE% <br> "
+#   fi
+# done <<< "$DISK_USAGE"
+
+# echo -e "Message Body: $MESSAGE"
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 # in project we keep it as 75
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
+
+while IFS= read -r line ;do
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+        MESSAGE+="High Disk usage on $PARTITION: $USAGE % <br>" # escaping
+    fi
+done <<< $DISK_USAGE
 
 echo -e "Message Body: $MESSAGE"
+
+#sh mail.sh "info@joindevops.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
+
+# TO_ADDRESS=$1
+# SUBJECT=$2
+# ALERT_TYPE=$3
+# MESSAGE_BODY=$4
+# IP_ADDRESS=$5
