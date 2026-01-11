af6907dd69c32c9428cd50e0497481894a33219d
pranathi.b99-cloud <pranathi.b99@gmail.com>
2026-01-11 16:35:37 +0530

    usage

--- 24-disk-usage.sh
+++ 24-disk-usage.sh
@@ -5,14 +5,14 @@ DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2 #in projects we keep it as 75
 MESSAGE=""
 IP_ADDRESS=$( curl -s http://169.254.169.254/latest/meta_data/local_ipv4)
-while IFS= read -r line
-
-do
-  USAGE=$(echo $line |awk '{ print $6 }' | cut -d "%" -f1)
-  PARTITION=$( echo $line |awk '{ print $7}')
-  if [ $USAGE -ge $DISK_THRESHOLD ] : then
-     MESSAGE+= " high disk usage $PARTITION=$USAGE % <br> "
 
 
-   fi
-done <<<  $DISK_USAGE
+
+while read -r line; do
+  USAGE=$(echo "$line" | awk '{ print $6 }' | cut -d "%" -f1)
+  PARTITION=$(echo "$line" | awk '{ print $7 }')
+  
+  if [ "$USAGE" -ge "$DISK_THRESHOLD" ]; then
+     MESSAGE+="high disk usage $PARTITION=$USAGE% <br> "
+  fi
+done <<< "$DISK_USAGE"
