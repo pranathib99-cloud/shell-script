ebb479162bb37b522b9a4a5d49472d0a67361cb8
pranathi.b99-cloud <pranathi.b99@gmail.com>
2026-01-13 13:37:33 +0530

    space

---
+++ cpu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DISK_THRESHOLD=80
+CPU_THRESHOLD=80
+RAM_THRESHOLD=80
+
+echo "===== Disk Usage Check ====="
+df -h | grep -v Filesystem | while read line
+do
+  USAGE=$(echo "$line" | awk '{print $5}' | cut -d% -f1)
+  PARTITION=$(echo "$line" | awk '{print $6}')
+
+  if [ "$USAGE" -ge "$DISK_THRESHOLD" ]; then
+    echo "Warning: Disk usage on $PARTITION is ${USAGE}%"
+  fi
+done
+
+echo
+echo "===== CPU Usage Check ====="
+CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
+
+CPU_USAGE=${CPU_USAGE%.*}
+
+if [ "$CPU_USAGE" -ge "$CPU_THRESHOLD" ]; then
+  echo "Warning: CPU usage is ${CPU_USAGE}%"
+fi
+
+echo
+echo "===== Memory Usage Check ====="
+MEM_USAGE=$(free | awk '/Mem/ {print ($3/$2)*100}')
+
+MEM_USAGE=${MEM_USAGE%.*}
+
+if [ "$MEM_USAGE" -ge "$RAM_THRESHOLD" ]; then
+  echo "Warning: Memory usage is ${MEM_USAGE}%"
+fi
